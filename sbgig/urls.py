4ed96241dc3525ee0d9444961ee04e87ad48c90a
Anton Gritsay <angri-n@yandex-team.ru>
2016-04-23 17:17:32 +0300

    sbgig: very simple "add comment" feature

--- sbgig/urls.py
+++ sbgig/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^(?P<slug>[^/]+)$', views.view_gig, name='view-gig'),
     url(r'^(?P<slug>[^/]+)/edit$', views.edit_gig, name='edit-gig'),
+    url(r'^(?P<slug>[^/]+)/add-comment$',
+        views.add_gig_comment, name='add-gig-comment'),
+    url(r'^add-song-comment/(?P<song_id>\d+)$',
+        views.add_song_comment, name='add-song-comment'),
 ]
--- sbgig/views.py
+++ sbgig/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib import messages
 from django.db.models import Count
 
+import sb.models
 import sbgig.models
 import sbgig.forms
 
@@ -39,3 +40,29 @@ def edit_gig(request, slug):
         return HttpResponseRedirect(reverse('sbgig:view-gig', args=[slug]))
     return render(request, 'sbgig/edit_gig.html',
                   {'gig': gig, 'form': form})
+
+
+def _add_comment(request, gig, song):
+    text = request.POST.get('body').strip()
+    if not text:
+        return HttpResponse(status=400)
+    comment_type = sbgig.models.Comment.CT_GIG_COMMENT
+    if song is not None:
+        comment_type = sbgig.models.Comment.CT_SONG_COMMENT
+    sbgig.models.Comment.objects.create(
+        gig=gig, song=song, author=request.user,
+        text=text, comment_type=comment_type
+    )
+    return JsonResponse({'result': 'ok'})
+
+
+@login_required
+def add_gig_comment(request, slug):
+    gig = get_object_or_404(sbgig.models.Gig, slug=slug)
+    return _add_comment(request, gig, song=None)
+
+
+@login_required
+def add_song_comment(request, song_id):
+    song = get_object_or_404(sb.models.Song, pk=song_id)
+    return _add_comment(request, song.gig, song)
