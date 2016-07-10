a9b920eee343ffcb80d115503397fed7806eac8b
angri <anton@angri.ru>
2016-07-11 01:33:29 +0300

    songbook/jinja2: separate past gigs from new in the nav bar

--- sbgig/jinja2env.py
+++ sbgig/jinja2env.py
@@ -1,5 +1,12 @@
+from django.utils import timezone
+
 import sbgig.models
 
 
-def all_gigs():
-    return sbgig.models.Gig.objects.order_by('date')
+def all_gigs(*, only_future=False, only_past=False):
+    gigs = sbgig.models.Gig.objects.order_by('date')
+    if only_future:
+        gigs = gigs.filter(date__gte=timezone.now().date())
+    if only_past:
+        gigs = gigs.filter(date__lt=timezone.now().date())
+    return gigs
